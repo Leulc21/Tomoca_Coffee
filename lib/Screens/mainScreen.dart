23c50173401fafff0e69ab04827d3c8b70332545
Leulc21 <leulchanie576@gmail.com>
2025-05-26 22:21:28 +0300

    icons aded pubspec

---
+++ lib/Screens/mainScreen.dart
@@ -0,0 +1,64 @@
+// ignore_for_file: file_names
+
+import 'package:flu_app/Models/Coffee_model.dart';
+import 'package:flu_app/Utils/colors.dart';
+import 'package:flutter/material.dart';
+
+class CoffeeAppMainScreen extends StatefulWidget {
+  const CoffeeAppMainScreen({super.key});
+
+  @override
+  State<CoffeeAppMainScreen> createState() => _CoffeeAppMainScreenState();
+}
+
+class _CoffeeAppMainScreenState extends State<CoffeeAppMainScreen> {
+  int indexMenu = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: menu[indexMenu]['destination'] as Widget,
+      backgroundColor: xbackgroundColor,
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: Row(
+            children: List.generate(menu.length, (index) {
+          Map items = menu[index];
+          bool isActive = indexMenu == index;
+          return Expanded(
+            child: InkWell(
+              onTap: () {
+                setState(() {
+                  indexMenu = index;
+                });
+              },
+              child: SizedBox(
+                height: 70,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const SizedBox(height: 20),
+                    Icon(
+                      items['icon'],
+                      color: isActive ? xprimaryColor : xsecondaryColor,
+                      size: 25,
+                    ),
+                    if (isActive) const SizedBox(height: 7),
+                    if (isActive)
+                      Container(
+                        height: 5,
+                        width: 15,
+                        decoration: BoxDecoration(
+                          color: xprimaryColor,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        })),
+      ),
+    );
+  }
+}
